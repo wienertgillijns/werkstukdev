fde6242928a20f1a65e300b52812a5a683d85564
Wienert Gillijns <wienert.gillijns@student.ehb.be>
2020-05-01 22:21:49 +0200

    Data ophalen en loggen

--- javascript.js
+++ javascript.js
@@ -23,11 +23,19 @@ fetch('entries.json')
   })
   .then(data => {
 	// Work with JSON data here
-	
+    //items van 0 eruit halen
+    var naam = data["items"]["1"]["name"];
+    var genre = data["items"]["1"]["genre-v2"];
+    var plaats = data["items"]["1"]["excerpt"];
+    var duur = data["items"]["1"]["video-length"];
 	
 
 
     console.log(data)
+    console.log(naam)
+    console.log(genre)
+    console.log(plaats)
+    console.log(duur)
   })
   .catch(err => {
     // Do something for an error here
