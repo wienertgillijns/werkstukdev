5918c9f4312ac18764c528034207f489f4412e9e
Wienert Gillijns <wienert.gillijns@student.ehb.be>
2020-05-02 20:36:12 +0200

    opruimen code

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
-
-
-
-fetch('entries.json')
+$(function(){
+    
+    fetch('entries.json')
   .then(response => {
     return response.json()
   })
@@ -11,92 +10,19 @@ fetch('entries.json')
   
    
 
-
-
-
-
-//     let getNaamVideo =  data["items"]["1"]["name"];
-//   $('section.videos').append(`
-//   <section class="apartevideo">
-//         <div id="genreVideo"></div>
-  
-//          <article class="infovideo">
-//              <h2 id="naamVideo">${getNaamVideo}"</h2>
-//             <p id="locatieVideo"></p>
-//             <p id="duratieVideo"></p>
-//         </article>
-//     </section>
-//       `);
-
-
-
-
+//array voor data ui de JSON file te halen
 var arrayLength = data["items"].length;
 for (var i = 0; i < arrayLength; i++) {
-    // console.log(data["items"][i]);
-    //Do something
-
-
-
-    //data eruit halen
-    // var naam = data["items"][i]["name"];
-    // var genre = data["items"][i]["genre-v2"];
-    // var plaats = data["items"][i]["excerpt"];
-    // var duur = data["items"][i]["video-length"];
-    // var opgenomen = data["items"][i]["recorded-at"];
-        // var leeftijd = data["items"][i]["age"];
-    
-    //items in html zetten
-    // document.getElementById("naamVideo").innerHTML = naam;
-    // document.getElementById("genreVideo").innerHTML = genre;
-    // document.getElementById("locatieVideo").innerHTML = plaats;
-    // document.getElementById("duratieVideo").innerHTML = duur;
-
-
-
-
-    
-
-
-
-    // loopvideos = "<section>" + data["items"][i]["genre-v2"] + "</section>";
-    // document.getElementById("alleVideos").innerHTML = loopvideos;
-
-//probeersel
-//     let getNaamVideo =  data["items"][i]["name"];
-//   $('section.videos').append(`
-//   <section class="apartevideo">
-//         <div id="genreVideo"></div>
-  
-//          <article class="infovideo">
-//              <h2 id="naamVideo">${getNaamVideo}"</h2>
-//             <p id="locatieVideo"></p>
-//             <p id="duratieVideo"></p>
-//         </article>
-//     </section>
-//       `);
-
-
-    // console.log(data)
-    // console.log(naam)
-    // console.log(genre)
-    // console.log(plaats)
-    // console.log(duur)
 
 }
 
 
-
-
-
-
-
-
-
-
+//videosectie weergeven
 var elkevideo = '';
 for (var i = 0; i < arrayLength; i++) {
      
+
+    //loop voor undefined eruit te halen
     var getAge = data["items"][i]["age"]; 
      var loopAgeNormal = '<div id="ageVideo">'+ getAge + '</div>';
      var loopAgeUndefined = '';
@@ -140,59 +66,134 @@ for (var i = 0; i < arrayLength; i++) {
              
      
             getDuratieVideo =  loopODuratieUndefined;
-     
-            
-     
                }else {
                 getDuratieVideo =  loopDuratieNormal;
                }
      
-
-
-
-
-
-
-
-
-
    elkevideo += '<section class="apartevideo videosapart"><section class="bovendeel"><div id="genreVideo">'+ data["items"][i]["genre-v2"] + '</div>' + getAge + '<img class="foto" src="'+ data["items"][i]["thumbnail"]["url"] +'"></section><article class="infovideo"><h2 id="naamVideo">'+ data["items"][i]["name"] +'</h2>'+ '<p id="locatieVideo">'+ getlocatieVideo + getOpgenomenVideo + getDuratieVideo +'</article></section>';
-
-   
-
-  
 
 var loopAparteVideos = document.getElementById("jsonarticles");
 loopAparteVideos.innerHTML = elkevideo;    
 
-
-
-
- 
 }
 
 
 
 
-// var url = data["items"][9]["link-to-video"]["url"];
+
 console.log(data);
-
-
-
-
   })
   .catch(err => {
     // Do something for an error here
   })
 
 
+
+
+
+
+
+
+
+//Bekijk meer button
+    $(".apartevideo").slice(0, 1).show(); 
+    
+    $("#bekijkMeer").click(function(e){ // click event for load more
+        
+        e.preventDefault();
+        $(".apartevideo:hidden").slice(0, 10).show(); // selecteren van 10 volgende elementen en hun zichtbaar maken
+        if($(".apartevideo:hidden").length == 0){ // nakijken of er verborgen elementen zijn
+            $("#bekijkMeer").text("Geen resultaten meer").addClass("noContent");
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+    
+});
+
+
+
+
+
+
+
+
+
+
+//     let getNaamVideo =  data["items"]["1"]["name"];
+//   $('section.videos').append(`
+//   <section class="apartevideo">
+//         <div id="genreVideo"></div>
   
+//          <article class="infovideo">
+//              <h2 id="naamVideo">${getNaamVideo}"</h2>
+//             <p id="locatieVideo"></p>
+//             <p id="duratieVideo"></p>
+//         </article>
+//     </section>
+//       `);
 
+
+
+    // loopvideos = "<section>" + data["items"][i]["genre-v2"] + "</section>";
+    // document.getElementById("alleVideos").innerHTML = loopvideos;
+
+//probeersel
+//     let getNaamVideo =  data["items"][i]["name"];
+//   $('section.videos').append(`
+//   <section class="apartevideo">
+//         <div id="genreVideo"></div>
   
+//          <article class="infovideo">
+//              <h2 id="naamVideo">${getNaamVideo}"</h2>
+//             <p id="locatieVideo"></p>
+//             <p id="duratieVideo"></p>
+//         </article>
+//     </section>
+//       `);
+
+
+    // console.log(data)
+    // console.log(naam)
+    // console.log(genre)
+    // console.log(plaats)
+    // console.log(duur)
 
 
 
 
+    // console.log(data["items"][i]);
+    //Do something
+
+
+
+    //data eruit halen
+    // var naam = data["items"][i]["name"];
+    // var genre = data["items"][i]["genre-v2"];
+    // var plaats = data["items"][i]["excerpt"];
+    // var duur = data["items"][i]["video-length"];
+    // var opgenomen = data["items"][i]["recorded-at"];
+        // var leeftijd = data["items"][i]["age"];
+    
+    //items in html zetten
+    // document.getElementById("naamVideo").innerHTML = naam;
+    // document.getElementById("genreVideo").innerHTML = genre;
+    // document.getElementById("locatieVideo").innerHTML = plaats;
+    // document.getElementById("duratieVideo").innerHTML = duur;
+
+
+
+
+    // var url = data["items"][9]["link-to-video"]["url"];
 
 
 
@@ -220,15 +221,3 @@ console.log(data);
     //   }
     // })
 // });
-$(function(){
-    $(".apartevideo").slice(0, 1).show(); // select the first ten
-    
-    $("#bekijkMeer").click(function(e){ // click event for load more
-        
-        e.preventDefault();
-        $(".apartevideo:hidden").slice(0, 10).show(); // select next 10 hidden divs and show them
-        if($(".apartevideo:hidden").length == 0){ // check if any hidden divs still exist
-            $("#bekijkMeer").text("Geen resultaten meer").addClass("noContent");
-        }
-    });
-});
